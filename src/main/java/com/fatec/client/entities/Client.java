5d77cb311e4ae7a4d205ab98a220046ec1f259c7
fehsato <fyonezawa14@gmail.com>
2023-05-30 10:57:26 -0300

    Banco de dados

--- src/main/java/com/fatec/client/entities/Client.java
+++ src/main/java/com/fatec/client/entities/Client.java
@@ -1,19 +1,41 @@
 package com.fatec.client.entities;
 
-public class Client {
-    private int id;
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_CLIENT")
+public class Client implements Serializable {
+        
+    @Id
+    private Integer id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
-    
-    public Client(int id, String email, String name) {
+
+    /* É obrigatório ter o contrutor vazio */ 
+    public Client() {
+    }
+
+
+    public Client(Integer id, String email, String name) {
         this.id = id;
         this.email = email;
         this.name = name;
+    
+    
+    /* Gerar get e set */ 
     }
-    public int getId() {
+    public Integer getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
     public String getName() {
@@ -27,6 +49,33 @@ public class Client {
     }
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    /* Gerar hashcode e equals
+     * Hashcode organiza e o equals determina que se o Id for igual é o mesmo objeto
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Client other = (Client) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
     
